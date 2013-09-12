721c3863e2f48077d14d443ff6c3f279d5064c43
jha.archana06@gmail.com <jha.archana06@gmail.com@8a5c6183-0a50-1417-d5b9-0589c9704ab7>
2013-09-12 12:24:13 +0000

    Added constructor

--- PRMS_PT04/src/java/sg/edu/nus/iss/phoenix/presenter/entity/Presenter.java
+++ PRMS_PT04/src/java/sg/edu/nus/iss/phoenix/presenter/entity/Presenter.java
@@ -22,6 +22,11 @@ public class Presenter implements Serializable,Cloneable{
         this.name="";
     }
     
+     public Presenter(String name){
+        this.id="";
+        this.name = name;
+    }
+    
     public Presenter(String id, String name) {
         this.id = id;
         this.name = name;
