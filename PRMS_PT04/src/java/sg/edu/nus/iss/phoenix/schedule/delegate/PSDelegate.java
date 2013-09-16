5519be0ae9bc8f205a632cdd9c883166b993c518
jeremy.gesmundo@gmail.com <jeremy.gesmundo@gmail.com@8a5c6183-0a50-1417-d5b9-0589c9704ab7>
2013-09-16 09:13:46 +0000

    added schedule delegate java file

---
+++ PRMS_PT04/src/java/sg/edu/nus/iss/phoenix/schedule/delegate/PSDelegate.java
@@ -0,0 +1,47 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sg.edu.nus.iss.phoenix.schedule.delegate;
+
+import java.util.ArrayList;
+import sg.edu.nus.iss.phoenix.schedule.entity.PSSearchObject;
+import sg.edu.nus.iss.phoenix.schedule.entity.ProgramSlot;
+import sg.edu.nus.iss.phoenix.schedule.service.ScheduleService;
+
+/**
+ *
+ * @author Jeremy
+ */
+public class PSDelegate {
+    public ArrayList<ProgramSlot> searchSchedule(PSSearchObject psso) {
+		//to do
+		return null;	
+	}
+	
+	public ArrayList<ProgramSlot> findProgramSlotByCriteria(PSSearchObject psso) {
+		//to do
+		return null;	
+	}
+	
+	public ProgramSlot findProgramSlot(String psName) {
+		//to do
+		return null;	
+		
+	}
+	public ArrayList<ProgramSlot> findAllProgramSlot() {
+		ScheduleService service = new ScheduleService();
+		return service.findAllProgramSlot();
+		
+	}
+	
+	public void insertProgramSlot(ProgramSlot ps) {
+		//to do
+	}
+	public void updateProgramSlot(ProgramSlot ps) {
+		//to do
+	}
+	public void deleteProgramSlot(ProgramSlot ps) {
+		//to do
+	}
+}
