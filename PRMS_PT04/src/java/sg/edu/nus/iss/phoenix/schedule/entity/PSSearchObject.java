526a88fdc28aefce666feb618bd46a741faae1c5
jeremy.gesmundo@gmail.com <jeremy.gesmundo@gmail.com@8a5c6183-0a50-1417-d5b9-0589c9704ab7>
2013-09-17 04:13:43 +0000

    updated sched search object

--- PRMS_PT04/src/java/sg/edu/nus/iss/phoenix/schedule/entity/PSSearchObject.java
+++ PRMS_PT04/src/java/sg/edu/nus/iss/phoenix/schedule/entity/PSSearchObject.java
@@ -1,38 +1,45 @@
 package sg.edu.nus.iss.phoenix.schedule.entity;
-import java.sql.Time;
-import java.util.Date;
-import sg.edu.nus.iss.phoenix.radioprogram.entity.RadioProgram;
 
+/**
+ *
+ * @author Jeremy
+ */
 
 public class PSSearchObject {
-                private Date dateOfProgram;
-                private Time startTime;
-                private String radioProgram;
-		public PSSearchObject() {
-			super();
-		}
-		public PSSearchObject(Date dateOfProgram,Time startTime,String radioProgram) {
-			super();
-			this.dateOfProgram = dateOfProgram;
-                        this.startTime = startTime;
-                        this.radioProgram= radioProgram;
-		}
-		public Date getDateOfProgram() {
-			return dateOfProgram;
-		}
-		public void setDateOfProgram(Date dateOfProgram) {
-			this.dateOfProgram = dateOfProgram;
-		}
-		public Time getStartTime() {
-			return startTime;
-		}
-		public void setStartTime(Time startTime) {
-			this.startTime = startTime;
-		}
-		public String getRadioProgramName() {
-			return radioProgram;
-		}
-		public void setRadioProgram(String radioProgram) {
-			this.radioProgram = radioProgram;
-		}
+        private String dateOfProgram;
+        private String startTime;
+        private String radioProgramName;
+
+        public PSSearchObject() {
+                super();
+        }
+        public PSSearchObject(String dateOfProgram,String startTime,String radioProgramName) {
+                super();
+                this.dateOfProgram = dateOfProgram;
+                this.startTime = startTime;
+                this.radioProgramName= radioProgramName;
+        }
+
+        public String getDateOfProgram() {
+            return this.dateOfProgram;
+        }
+
+        public void setDateOfProgram(String dateOfProgram) {
+             this.dateOfProgram = dateOfProgram;
+        }
+
+        public String getStartTime() {
+             return this.startTime;
+        }
+
+        public void setStartTime(String startTime){
+             this.startTime = startTime;
+        }
+
+        public String getRadioProgramName() {
+              return this.radioProgramName;
+        }
+        public void setRadioProgramName(String radioProgramName) {
+              this.radioProgramName = radioProgramName;
+        }
 }
